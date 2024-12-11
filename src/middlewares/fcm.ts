d266a0361915047d8f2f608e8a12f5a6a9001b8c
Di-nobi <architectstan01@gmail.com>
2024-12-11 21:30:33 +0100

    (feat) fixed the error causing file not to upload

--- src/middlewares/fcm.ts
+++ src/middlewares/fcm.ts
@@ -1,14 +1,14 @@
 
-import { initializeApp, cert, ServiceAccount } from 'firebase-admin/app';
-import { getMessaging } from 'firebase-admin/messaging';
+// import { initializeApp, cert, ServiceAccount } from 'firebase-admin/app';
+// import { getMessaging } from 'firebase-admin/messaging';
 //import * as serviceAccount from '/home/ubuntu/pepoz_backend/src/pepoz-39817-firebase-adminsdk-2l6ki-e70aeb6401.json'
-import * as serviceAccount from '../pepoz-39817-firebase-adminsdk-2l6ki-e70aeb6401.json'
-process.env.GOOGLE_APPLICATION_CREDIENTIALS;
+// import * as serviceAccount from '../pepoz-39817-firebase-adminsdk-2l6ki-e70aeb6401.json'
+// process.env.GOOGLE_APPLICATION_CREDIENTIALS;
 
-initializeApp({
-  credential: cert(serviceAccount as ServiceAccount),
-  projectId: 'pepoz-39817'
-})
+// initializeApp({
+//   credential: cert(serviceAccount as ServiceAccount),
+//   projectId: ''
+// })
 
 // Function to send FCM message
 export async function sendFCMNotification(token: string, title: any, body: any) {
